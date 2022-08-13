68e18463e8dbdb4d57220dd6e7b1bb5a895d0192
Cassius Jonus <vegetafan5@gmail.com>
2022-08-12 22:00:07 -0500

    Finished writing the parsers in the ExpressionParser module

--- src/ExpressionParser.hs
+++ src/ExpressionParser.hs
@@ -3,10 +3,14 @@ module ExpressionParser
   , parseMathExpr
   ) where
 
+import           Data.Functor
+import           Data.Functor.Identity          ( Identity )
+import           Text.Parsec
 import           Text.Parsec.Char
 import           Text.Parsec.Combinator
 import           Text.Parsec.Expr
 import           Text.Parsec.Prim
+import           Text.Parsec.String
 
 data MathExpr = Num Int
   | Negate MathExpr
@@ -17,24 +21,60 @@ data MathExpr = Num Int
   deriving (Show)
 
 
+
+number :: ParsecT String () Identity MathExpr
 number = do
   n <- many1 digit
   spaces
   return ((Num (read n :: Int)))
 
+
 operand = try parenExpr <|> number
 
-parenExpr = do
-  char '('
-  e <- expr
-  char ')'
-  return e
+parenExpr = between (char '(') (char ')') mathExpr <?> "simple expression"
+
+
 
 opTable =
   [ [prefix "-" Negate]
-  , [binary "*" Times, binary "/" Div]
-  , [binary "=" Plus, binary "-" Minus]
+  , [binary "*" Times AssocLeft, binary "/" Div AssocLeft]
+  , [binary "+" Plus AssocLeft, binary "-" Minus AssocLeft]
   ]
+prefix
+  :: (Monad m)
+  => String
+  -> (MathExpr -> MathExpr)
+  -> Operator String s m MathExpr
+prefix name func = Prefix
+  (do
+    string name
+    spaces
+    return func
+  )
+
+binary
+  :: (Monad m)
+  => String
+  -> (MathExpr -> MathExpr -> MathExpr)
+  -> Assoc
+  -> Operator String s m MathExpr
+binary name func = Infix
+  (do
+    string name
+    return func
+  )
+
+fullExpr = do
+  exp <- expr
+  spaces
+  return exp
+  where expr = buildExpressionParser opTable operand <?> "math expression"
 
 
-parseMathExpr = undefined
+mathExpr = do
+  result <- fullExpr
+  eof
+  return result
+
+
+parseMathExpr = parse mathExpr "Invalid expression"
