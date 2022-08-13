a3caaa9c146612daa3bbcc5235b2d251b0ecbcdd
Cassius Jonus <vegetafan5@gmail.com>
2022-08-13 17:41:22 -0500

    Implemented Calc

--- app/CalcRunner.hs
+++ app/CalcRunner.hs
@@ -1,2 +1,29 @@
-module CalcRunner (run) where
+module CalcRunner
+    ( run
+    ) where
+
+import           CalculatorUtils
+
+import           Data.Char                      ( toLower )
+import           Text.Parsec                    ( ParseError )
+
+
+run :: IO ()
+run = do
+    putStrLn
+        "Enter an arithmetic expression or press \"q\" to quit(Includes addition, subtraction, multiplication, and division.)"
+    expression <- getLine
+    if map toLower expression == "q"
+        then do
+            putStrLn "Exiting Calculator"
+            return ()
+        else do
+            let expr = parseMathExpr expression
+            case expr of
+                Left e -> do
+                    putStrLn $ "Invalid expression."
+                    run
+                Right mathExpr -> do
+                    putStrLn $ show (evaluate mathExpr)
+                    run
 
